e52339ed6ea8552c955bb484add090e43ef7a629
zvs08 <vsev08@gmail.com>
2022-01-18 14:18:49 -0500

    save every epoch

--- lib/train/trainers/base_trainer.py
+++ lib/train/trainers/base_trainer.py
@@ -90,7 +90,7 @@ class BaseTrainer:
                         else:
                             self.lr_scheduler.step(epoch - 1)
                     # only save the last 10 checkpoints
-                    save_every_epoch = getattr(self.settings, "save_every_epoch", False)
+                    save_every_epoch = getattr(self.settings, "save_every_epoch", True)
                     if epoch > (max_epochs - 10) or save_every_epoch or epoch % 100 == 0:
                         if self._checkpoint_dir:
                             if self.settings.local_rank in [-1, 0]:
