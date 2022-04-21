e720cd795e06767a10c4b6afd70cabe4b1f72baf
Alexander Ivkin <a@ivkin.dev>
2022-04-21 12:30:07 -0400

    Added function description

--- tracking/ORT_stark_st_backbone.py
+++ tracking/ORT_stark_st_backbone.py
@@ -17,6 +17,8 @@ from torch.utils.mobile_optimizer import optimize_for_mobile
 from lib.utils.misc import NestedTensor
 
 
+# img_arr (H,W,3)
+# amask_arr (H,W)
 def process(img_arr, amask_arr):
     mean = torch.tensor([0.485, 0.456, 0.406]).view((1, 3, 1, 1))
     std = torch.tensor([0.229, 0.224, 0.225]).view((1, 3, 1, 1))
